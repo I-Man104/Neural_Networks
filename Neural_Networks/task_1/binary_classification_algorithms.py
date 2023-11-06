b13510cf939cd01f3164cfa46863d4d3cd5c9555
I-Man104 <ayman.m.h104@gmail.com>
2023-11-06 16:17:24 +0200

    fixed bug?

--- Neural_Networks/task_1/binary_classification_algorithms.py
+++ Neural_Networks/task_1/binary_classification_algorithms.py
@@ -4,11 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
+
 def preprocess():
-  data = pd.read_csv('D:/Neural_Networks/dry_bean_dataset.csv')
-  data['MinorAxisLength'].interpolate(method='linear', inplace=True)
-  df = pd.DataFrame(data)
-  return df
+    data = pd.read_csv('./dataset/dry_bean_dataset.csv')
+    data['MinorAxisLength'].interpolate(method='linear', inplace=True)
+    df = pd.DataFrame(data)
+    return df
+
 
 def single_preceptron(learning_rate, epochs, Mse_threshold, features, classes, bias=False):
     data = preprocess()
@@ -18,7 +20,8 @@ def single_preceptron(learning_rate, epochs, Mse_threshold, features, classes, b
     np.random.seed(0)
     weights = np.random.rand(X.shape[1])
     biass = np.random.rand() if bias else 0
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.25, random_state=0)
 
     # Perceptron learning
     for epoch in range(epochs):
@@ -36,8 +39,8 @@ def single_preceptron(learning_rate, epochs, Mse_threshold, features, classes, b
     # Calculate accuracy on the test set
     error = 0
     for i in range(len(y_test)):
-        prediction = np.sign(np.dot(xi,weights)+biass)
-        prediction= -1 if prediction <=0 else 1
+        prediction = np.sign(np.dot(xi, weights)+biass)
+        prediction = -1 if prediction <= 0 else 1
         if prediction == y_test[i]:
             continue
         else:
@@ -47,33 +50,33 @@ def single_preceptron(learning_rate, epochs, Mse_threshold, features, classes, b
     return weights, biass, X_train, X_test, y_train, y_test
 
 
-#def train_model(learning_rate,epochs,Mse_threshold,features,classes,bias=False):
- # pass
+# def train_model(learning_rate,epochs,Mse_threshold,features,classes,bias=False):
+# pass
 
 
-
-    
-def plotting(weights,bias,X_train,X_test,y_train,y_test):
-  plt.scatter(X_test[y_test == 0][:, -1], X_test[y_test == 0][:, 1], label=f'Class BOMBAY')
-  plt.scatter(X_test[y_test == 1][:, -1], X_test[y_test == 1][:, 1], label=f'Class SIRA')
-  w0, w1 = weights
+def plotting(weights, bias, X_train, X_test, y_train, y_test):
+    plt.scatter(X_test[y_test == 0][:, -1],
+                X_test[y_test == 0][:, 1], label=f'Class BOMBAY')
+    plt.scatter(X_test[y_test == 1][:, -1],
+                X_test[y_test == 1][:, 1], label=f'Class SIRA')
+    w0, w1 = weights
 #   bias = bias_value
 
-  x_range = np.linspace(X_test[:, 0].min(), X_test[:, 0].max(), num=100)
-  y_range = (-bias - w0 * x_range) / w1
-  plt.plot(x_range, y_range, label='Decision Boundary', linestyle='--')
-  plt.xlabel('Feature 1')
-  plt.ylabel('Feature 2')
-  plt.legend()
-  plt.show()
-  
-weights,bias_value,X_train,X_test,y_train,y_test = single_preceptron(0.1, 100, 0.01, ['Area','Perimeter'], ['BOMBAY','SIRA'], bias=False)
+    x_range = np.linspace(X_test[:, 0].min(), X_test[:, 0].max(), num=100)
+    y_range = (-bias - w0 * x_range) / w1
+    plt.plot(x_range, y_range, label='Decision Boundary', linestyle='--')
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
+    plt.legend()
+    plt.show()
 
-plotting(weights,bias_value,X_train,X_test,y_train,y_test)   
 
-    
-    
-    
+weights, bias_value, X_train, X_test, y_train, y_test = single_preceptron(
+    0.1, 100, 0.01, ['Area', 'Perimeter'], ['BOMBAY', 'SIRA'], bias=False)
+
+plotting(weights, bias_value, X_train, X_test, y_train, y_test)
+
+
 # def single_preceptron(learning_rate, epochs, Mse_threshold, features, classes, bias=False):
 #   data = preprocess()
 #   data = data[(data['Class'] == 'BOMBAY') | (data['Class'] == 'SIRA')]
